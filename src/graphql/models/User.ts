95bc35646bf80687dbeab035f08a0941af8ef792
Kemal Onur İlkılıç <kemalonur.ilkilic@bahcesehir.edu.tr>
2025-03-12 01:45:22 +0300

    refactor: remove AppController and AppService; integrate GraphQL module

---
+++ src/graphql/models/User.ts
@@ -0,0 +1,14 @@
+import { Field, ObjectType, Int } from '@nestjs/graphql';
+
+@ObjectType()
+export class User {
+  // we only do this for integer
+  @Field((type) => Int)
+  id: number;
+
+  @Field()
+  username: string;
+
+  @Field({ nullable: true })
+  displayName?: string;
+}
